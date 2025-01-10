e454677bc4e90a291c459f65b1f0dc8c9516a3b3
Rahmanyahya <rahmanyahya647@gmail.com>
2025-01-10 21:06:21 +0700

    modified:   package-lock.json modified:   package.json 	new file:   src/database/cloudinary/cloudinary.ts 	modified:   src/database/cloudinary/conection.ts 	new file:   src/infra/global.config.ts

---
+++ src/infra/global.config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const config = {
+    Server: {
+        port: process.env.SERVER_PORT || 3030,
+        prefix: process.env.SERVER_PREFIX
+    },
+    Mailer: {
+        gmail: process.env.GMAIL,
+        password: process.env.PASSWORD
+    },
+    Redis: {
+        redis_connection: process.env.REDIS_URL_CONECTION
+    },
+    Jwt: {
+        secret: process.env.JWT_SECRET
+    },
+    Cloudinary: {
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET,
+        folder_name: process.env.CLOUD_FOLDER_NAME
+    }
+}
+
+
